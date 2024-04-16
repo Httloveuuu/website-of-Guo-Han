762ffc6f79c9a525fc15a0ee88756ebafc7483d7
Httloveuuu <89408874+Httloveuuu@users.noreply.github.com>
2024-04-16 17:50:05 +0800

    删除无用的js文件

--- js/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('mobile-nav-btn').addEventListener('click', function() {
-    var myElement = document.getElementById('mobile-menu');
-    if (myElement.style.display === 'none') {
-        myElement.style.display = 'block';
-    } else {
-        myElement.style.display = 'none';
-    }
-});
